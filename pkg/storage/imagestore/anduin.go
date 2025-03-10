ec321717000c361d01b04fbeae5dea1b77295c6a
Tuan Vuong <vanhtuan0409@gmail.com>
2025-03-10 12:30:51 +0700

    fix: presign url ext to use per repo lock

--- pkg/storage/imagestore/anduin.go
+++ pkg/storage/imagestore/anduin.go
@@ -2,25 +2,24 @@ package imagestore
 
 import (
 	"net/http"
-	"time"
 
 	godigest "github.com/opencontainers/go-digest"
 )
 
 func (is *ImageStore) GenPresignLink(r *http.Request, repo string, digest godigest.Digest) (string, error) {
-	var lockLatency time.Time
-
 	if err := digest.Validate(); err != nil {
 		return "", err
 	}
 
-	is.RLock(&lockLatency)
-	defer is.RUnlock(&lockLatency)
+	presignedLink := ""
+	err := is.lock.WithRepoReadLock(repo, func() error {
+		binfo, err := is.originalBlobInfo(repo, digest)
+		if err != nil {
+			return err
+		}
 
-	binfo, err := is.originalBlobInfo(repo, digest)
-	if err != nil {
-		return "", err
-	}
-
-	return is.storeDriver.RedirectURL(r, binfo.Path())
+		presignedLink, err = is.storeDriver.RedirectURL(r, binfo.Path())
+		return err
+	})
+	return presignedLink, err
 }
